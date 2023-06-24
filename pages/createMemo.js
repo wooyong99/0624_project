10d6cf108447acbe06371ca26fb859ee12307e36
wooyong99 <wulovesk@naver.com>
2023-06-24 19:57:33 +0900

    로그인 성공 시 라우터 설정

---
+++ pages/createMemo.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+
+export default function CreateMemo() {
+  
+  return (
+    <main>
+      create memo
+    </main>
+  )
+}
--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,26 @@
 import { useState } from "react"
+import { useRouter } from "next/router"
 
 export default function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
+  const router = useRouter()
   const loginCheck = () => {
-    console.log(email);
+    console.log(email)
     console.log(password)
+
+    if(email === "wulovesk@naver.com") {
+      if(password === "123"){
+        alert("login success")
+        router.push("/createMemo")
+      }else{
+        alert("Not Found password")
+        return
+      }
+    }else{
+      alert("Not Found email")
+      return
+    }
   }
   return (
     <main>

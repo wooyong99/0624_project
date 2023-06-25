54243662dbb5efed35c9389d4849954a858bf927
wooyong99 <wulovesk@naver.com>
2023-06-25 12:41:46 +0900

    네비게이션 바 구현

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,6 @@ import { useRouter } from "next/router"
 export default function SignUp() {
   return (
     <main>
-      <a href="/signUp">Sign up</a><br/>
-      <a href="/login">Login</a><br/>
-      <a href="/memoList">Memo List</a><br/>
     </main>
   )
 }
